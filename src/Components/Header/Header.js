de3b67732c1bc0cbff0e4669b9239f95ec97bd28
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-12-24 09:05:16 +0600

    working on navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,38 +4,76 @@ import './Header.css'
 const Header = () => {
     return (
         <>
-            <div className='w-full bg-sky-500 h-64 grid gap-2 grid-cols-8'>
-                <div className=''>
-                    <p className='text-xl  nav-bar '>Home
-                        <div className=' h-48 drop-down bg-red-50' style={{ width: "300px" }}>
-                            heklehrl
-                        </div>
+            <div className='w-full flex justify-center' style={{ backgroundColor: "#455a64" }}>
+                <div className='w-4/5  h-64 grid gap-2 grid-cols-8  '>
+                    {/* <div></div> */}
+                    <div className=''>
+                        <p className='text-xl  nav-bar  '>Home
+                            <div className=' h-48 drop-down bg-red-50' style={{ width: "200px" }}>
+                                drop down
+                            </div>
 
-                    </p>
+                        </p>
+
+                    </div>
+                    <div>
+                        <p className='text-xl  nav-bar '>About us
+                            <div className=' h-48 drop-down bg-red-50' style={{ width: "300px" }}>
+                                drop down
+                            </div>
+
+                        </p>
+                    </div>
+                    <div>
+                        <p className='text-xl  nav-bar '>Admission
+                            <div className=' h-48 drop-down bg-red-50' style={{ width: "300px" }}>
+                                drop down
+                            </div>
+
+                        </p>
+                    </div>
+                    <div>
+                        <p className='text-xl  nav-bar '>IQAC
+                            <div className=' h-48 drop-down bg-red-50' style={{ width: "300px" }}>
+                                drop down
+                            </div>
+
+                        </p>
+                    </div>
+                    <div>
+                        <p className='text-xl  nav-bar '>Faculty
+                            <div className=' h-48 drop-down bg-red-50' style={{ width: "300px" }}>
+                                drop down
+                            </div>
+
+                        </p>
+                    </div>
+                    <div>
+                        <p className='text-xl  nav-bar '>Student
+                            <div className=' h-48 drop-down bg-red-50' style={{ width: "300px" }}>
+                                drop down
+                            </div>
+
+                        </p>
+                    </div>
+                    <div>
+                        <p className='text-xl  nav-bar '>Library
+                            <div className=' h-48 drop-down bg-red-50' style={{ width: "300px" }}>
+                                drop down
+                            </div>
+
+                        </p>
+                    </div>
+                    <div>
+                        <p className='text-xl  nav-bar '>Login
+                            <div className=' h-48 drop-down bg-red-50' style={{ width: "300px" }}>
+                                drop down
+                            </div>
+
+                        </p>
+                    </div>
 
                 </div>
-                <div>
-                    <p className='text-xl'>About us</p>
-                </div>
-                <div>
-                    <p className='text-xl'>Admission</p>
-                </div>
-                <div>
-                    <p className="text-xl">IQAC</p>
-                </div>
-                <div>
-                    <p className='text-xl'>Facaulty</p>
-                </div>
-                <div>
-                    <p className='text-xl'>Student</p>
-                </div>
-                <div>
-                    <p className='text-xl' >Library</p>
-                </div>
-                <div>
-                    <p className='text-xl'>Login</p>
-                </div>
-                <div></div>
             </div>
         </>
     );
