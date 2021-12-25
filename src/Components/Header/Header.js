ddbb144958a59b436433d43e443d4fc6665784ff
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-12-25 13:53:31 +0600

    fixed label

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import { GiHamburgerMenu } from 'react-icons/gi'
 import { MdOutlineClose } from 'react-icons/md';
 import aboutUs, { admission, faculty, facultyMembers, studentsPortal } from './Navbar-drop-down-items/drop-down-items';
 import logo from '../../img/lulogo.png'
-import label from '../../img/lulabel.png'
+import label from '../../img/lulabelwhite.png'
 const Header = () => {
 
     const FullNav =
@@ -144,12 +144,12 @@ const Header = () => {
             <div className='w-full' style={{ backgroundColor: "#455a64" }}>
                 <div className='lg:w-4/5 mx-auto sm:w-full'>
                     <div className='grid lg:grid-cols-3 sm:grid-cols-2 mb-8  grid-cols-2 border-b border-slate-900'>
-                        <div className="lg:flex  sm:flex-column md:flex-column  lg:col-span-2 gap-4 my-8 ">
-                            <div className=' w-1/4'>
+                        <div className="lg:flex  sm:flex-column md:flex-column  lg:col-span-2  my-8 ">
+                            <div className=' w-1/4 '>
                                 <img src={logo} alt="logo" style={{ width: "150px" }}></img>
                             </div>
-                            <div className=' w-full flex '>
-                                <img src={label} alt="label" className='w-4/5 ' ></img>
+                            <div className=' w-full flex justify-start items-center  '>
+                                <img src={label} alt="label" className=' label' ></img>
                             </div>
                         </div>
                         <div className='  hamburger lg:flex lg:justify-end  lg:items-start sm:flex sm:justify-end  sm:items-start '>
