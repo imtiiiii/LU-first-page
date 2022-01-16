ab57979163c0099953b340da41b5af9a4b22c322
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-01-16 21:44:36 +0600

    created hero section file

---
+++ src/Components/Home/HeroSection/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <>
+            <h1>This is hero section</h1>
+        </>
+    );
+};
+
+export default HeroSection;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroSection from './HeroSection/HeroSection';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Welcome to home </h1>
+            <HeroSection></HeroSection>
+        </>
+    );
+};
+
+export default Home;
