7395ed3cfdc93ddedb86a7dcb12bf15963644c3f
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-01-25 10:26:49 +0600

    fixing desing/layout of the page

--- src/Components/Home/SidePart/SidePart.js
+++ src/Components/Home/SidePart/SidePart.js
@@ -15,39 +15,39 @@ const SidePart = () => {
     ]
     return (
         <>
-            <div className='bg-[#c3d136]'>
+            <div className='bg-[#c3d136] my-5 text-center py-4'>
                 <h2>Admission Going On <br />Spring-22</h2>
             </div>
             <div>
                 <img src={circular} alt="circular" />
             </div>
-            <div>
+            <div className='bg-[#fff]'>
                 <h3>Forms and Downloads</h3>
                 <p>Click here</p>
             </div>
-            <div>
+            <div className='bg-[#fff]'>
                 <h3>Contact</h3>
                 <p>Click here</p>
             </div>
-            <div>
+            <div className='bg-[#fff]'>
                 <h3>Forms and Downloads</h3>
                 <p>Academic Calendar</p>
             </div>
             <div>
                 <h1>Notice Board</h1>
-                <div className='flex'>
+                <div className='flex bg-[#fff]'>
                     <div><h6>19 <br /> Jan</h6></div>
                     <div><p>Notice for Re-registration for Vac Certificate</p></div>
                 </div>
-                <div className='flex'>
+                <div className='flex bg-[#fff]'>
                     <div><h6>13 <br /> Jan</h6></div>
                     <div><p>Notice </p></div>
                 </div>
-                <div className='flex'>
+                <div className='flex bg-[#fff]'>
                     <div><h6>13 <br /> Jan</h6></div>
                     <div><p>Notice</p></div>
                 </div>
-                <div className='flex'>
+                <div className='flex bg-[#fff]'>
                     <div><h6>9 <br /> Jan</h6></div>
                     <div><p>Regarding Final Make-Up Exam of Summer-2021</p></div>
                 </div>
