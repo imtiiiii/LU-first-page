bd820896dddc098c33c7f6a40aaa765a4f381e2c
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-01-31 22:37:29 +0600

    working on notice board

--- src/Components/Home/SidePart/SidePart.js
+++ src/Components/Home/SidePart/SidePart.js
@@ -15,27 +15,27 @@ const SidePart = () => {
     ]
     return (
         <>
-            <div className='bg-[#c3d136] my-5 text-center py-4'>
-                <h2>Admission Going On <br />Spring-22</h2>
+            <div className='bg-[#c3d136] mb-8 text-center py-4'>
+                <h2 className='text-4xl text-[#fff]'>Admission Going On <br />Spring-22</h2>
             </div>
             <div>
                 <img src={circular} alt="circular" />
             </div>
-            <div className='bg-[#fff]'>
-                <h3>Forms and Downloads</h3>
-                <p>Click here</p>
+            <div className='bg-[#fff] my-7 '>
+                <h3 className='text-2xl text-[#c3d136] mb-4'>Forms and Downloads</h3>
+                <p className='text-base font-medium'>Click here</p>
+            </div>
+            <div className='bg-[#fff] my-7'>
+                <h3 className='text-2xl text-[#c3d136] mb-4'>Contact</h3>
+                <p className='text-base font-medium'>Click here</p>
+            </div>
+            <div className='bg-[#fff] my-7'>
+                <h3 className='text-2xl text-[#c3d136] mb-4'>Forms and Downloads</h3>
+                <p className='text-base font-medium'>Click here</p>
             </div>
             <div className='bg-[#fff]'>
-                <h3>Contact</h3>
-                <p>Click here</p>
-            </div>
-            <div className='bg-[#fff]'>
-                <h3>Forms and Downloads</h3>
-                <p>Academic Calendar</p>
-            </div>
-            <div>
-                <h1>Notice Board</h1>
-                <div className='flex bg-[#fff]'>
+                <h1 className='text-2xl text-[#c3d136] mb-4'>Notice Board</h1>
+                <div className='flex '>
                     <div><h6>19 <br /> Jan</h6></div>
                     <div><p>Notice for Re-registration for Vac Certificate</p></div>
                 </div>
