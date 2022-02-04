697250235e95557d617f626cf8802bde4fe9bdba
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-02-04 15:54:48 +0600

    working on footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <div className='w-[50%] mx-auto flex border-2 border-solid h-[1000px] bg-[#455a64]'>
-                <h1>hello</h1>
+        <div className='w-[100%]'>
+            <div className='  h-[200px] bg-[#455a64]'>
+                <div className=' w-[70%]  mx-auto '>
+                    <h1 className='text-lg text-[#c3d136] my-6'>Contact</h1>
+                    <h5 className='text-base ' style={{ color: "rgba(255, 255, 255, 0.7)" }}>Leading University</h5>
+                    <h5 className='text-base text-[#fff]' style={{ color: "rgba(255, 255, 255, 0.7)" }}>Ragibnagar, South Surma, Sylhet-3112</h5>
+                    <h5 className='text-base text-[#fff]' style={{ color: "rgba(255, 255, 255, 0.7)" }}>Phone: 01313084499.</h5>
+                    <h5 className='text-base text-[#fff]' style={{ color: "rgba(255, 255, 255, 0.7)" }}>Email: info@lus.ac.bd</h5>
+
+                </div>
             </div>
             <div>
 
